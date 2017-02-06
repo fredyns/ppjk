795b0d1ad46788f612e357b349a0de222c52a70f
Fredy Nurman Saleh <email@fredyns.net>
2017-02-06 15:56:27 +0700

    wip job-container form

--- views/job-container/_form.php
+++ views/job-container/_form.php
@@ -3,8 +3,13 @@
 use yii\bootstrap\ActiveForm;
 use yii\helpers\Html;
 use yii\helpers\StringHelper;
+use yii\helpers\Url;
 use cornernote\returnurl\ReturnUrl;
 use dmstr\bootstrap\Tabs;
+use app\models\form\JobContainerForm;
+use yii\helpers\ArrayHelper;
+use kartik\widgets\Select2;
+use yii\web\JsExpression;
 
 /**
  * @var yii\web\View $this
@@ -17,79 +22,180 @@ use dmstr\bootstrap\Tabs;
 
     <?php
     $form = ActiveForm::begin([
-        'id' => 'JobContainer',
-        'layout' => 'horizontal',
-        'enableClientValidation' => true,
-        'errorSummaryCssClass' => 'error-summary alert alert-error'
+            'id' => 'JobContainer',
+            'layout' => 'horizontal',
+            'enableClientValidation' => true,
+            'errorSummaryCssClass' => 'error-summary alert alert-error'
     ]);
-    
+
     echo Html::hiddenInput('ru', ReturnUrl::getRequestToken());
     ?>
 
     <div class="">
+
+        <?php $this->beginBlock('ShippingInstruction'); ?>
+
+        <div id="input-si">
+
+            <!-- attribute newSi -->
+            <?=
+            $form->field($model, 'newSi')->radio(JobContainerForm::optsNewSi());
+            ?>
+
+            <div id="input-picksi">
+
+                <!-- attribute shippingInstruction_id -->
+                <?=
+                    $form
+                    ->field($model, 'shippingInstruction_id')
+                    ->widget(Select2::classname(),
+                        [
+                        'initValueText' => ArrayHelper::getValue($model, 'shippingInstruction.number', '-'),
+                        'options' => ['placeholder' => 'mencari shipping instruction ...'],
+                        'pluginOptions' => [
+                            'minimumInputLength' => 3,
+                            'language' => [
+                                'errorLoading' => new JsExpression("function () { return 'menunggu hasil...'; }"),
+                            ],
+                            'ajax' => [
+                                'url' => Url::to(['/api/shipping-instruction/list']),
+                                'dataType' => 'json',
+                                'data' => new JsExpression('function(params) { return {q:params.term}; }')
+                            ],
+                            'escapeMarkup' => new JsExpression('function (markup) { return markup; }'),
+                            'templateResult' => new JsExpression('function(item) { return item.text; }'),
+                            'templateSelection' => new JsExpression('function (item) { return item.text; }'),
+                        ],
+                ]);
+                ?>
+
+            </div>
+
+            <div id="input-newsi">
+
+                <!-- attribute shippingInstructionNumber -->
+                <?=
+                $form->field($model, 'shippingInstructionNumber')->textInput(['maxlength' => true])
+                ?>
+
+                <div id="input-shipper">
+
+                    <!-- attribute shipperId -->
+                    <?=
+                        $form
+                        ->field($model, 'shipperId')
+                        ->widget(Select2::classname(),
+                            [
+                            'initValueText' => ArrayHelper::getValue($model, 'shippingInstruction.shipper.name', '-'),
+                            'options' => ['placeholder' => 'mencari perusahaan ...'],
+                            'pluginOptions' => [
+                                'allowClear' => true,
+                                'tags' => true,
+                                'minimumInputLength' => 3,
+                                'language' => [
+                                    'errorLoading' => new JsExpression("function () { return 'menunggu hasil...'; }"),
+                                ],
+                                'ajax' => [
+                                    'url' => Url::to(['/api/company-profile/list']),
+                                    'dataType' => 'json',
+                                    'data' => new JsExpression('function(params) { return {q:params.term}; }')
+                                ],
+                                'escapeMarkup' => new JsExpression('function (markup) { return markup; }'),
+                                'templateResult' => new JsExpression('function(item) { return item.text; }'),
+                                'templateSelection' => new JsExpression('function (item) { return item.text; }'),
+                            ],
+                    ]);
+                    ?>
+
+                    <div id="input-shipperdetail">
+
+                        <!-- attribute shipperAddress -->
+                        <?=
+                        $form->field($model, 'shipperAddress')->textarea(['rows' => 3])
+                        ?>
+
+                        <!-- attribute shipperPhone -->
+                        <?=
+                        $form->field($model, 'shipperPhone')->textInput(['maxlength' => true])
+                        ?>
+
+                        <!-- attribute shipperEmail -->
+                        <?=
+                        $form->field($model, 'shipperEmail')->textInput(['maxlength' => true])
+                        ?>
+
+                        <!-- attribute shipperNpwp -->
+                        <?=
+                        $form->field($model, 'shipperNpwp')->textInput(['maxlength' => true])
+                        ?>
+
+                    </div>
+
+                </div>
+
+                <!-- attribute shippingId -->
+                <?=
+                    $form
+                    ->field($model, 'shippingId')
+                    ->widget(Select2::classname(),
+                        [
+                        'initValueText' => ArrayHelper::getValue($model, 'shippingInstruction.shipping.name', '-'),
+                        'options' => ['placeholder' => 'mencari pelayaran ...'],
+                        'pluginOptions' => [
+                            'allowClear' => true,
+                            'tags' => true,
+                            'minimumInputLength' => 3,
+                            'language' => [
+                                'errorLoading' => new JsExpression("function () { return 'menunggu hasil...'; }"),
+                            ],
+                            'ajax' => [
+                                'url' => Url::to(['/api/shipping/list']),
+                                'dataType' => 'json',
+                                'data' => new JsExpression('function(params) { return {q:params.term}; }')
+                            ],
+                            'escapeMarkup' => new JsExpression('function (markup) { return markup; }'),
+                            'templateResult' => new JsExpression('function(item) { return item.text; }'),
+                            'templateSelection' => new JsExpression('function (item) { return item.text; }'),
+                        ],
+                ]);
+                ?>
+
+                <!-- attribute destinationId -->
+                <?=
+                    $form
+                    ->field($model, 'destinationId')
+                    ->widget(Select2::classname(),
+                        [
+                        'initValueText' => ArrayHelper::getValue($model, 'shippingInstruction.destination.name', '-'),
+                        'options' => ['placeholder' => 'mencari pelabuhan ...'],
+                        'pluginOptions' => [
+                            'allowClear' => true,
+                            'tags' => true,
+                            'minimumInputLength' => 3,
+                            'language' => [
+                                'errorLoading' => new JsExpression("function () { return 'menunggu hasil...'; }"),
+                            ],
+                            'ajax' => [
+                                'url' => Url::to(['/api/container-port/list']),
+                                'dataType' => 'json',
+                                'data' => new JsExpression('function(params) { return {q:params.term}; }')
+                            ],
+                            'escapeMarkup' => new JsExpression('function (markup) { return markup; }'),
+                            'templateResult' => new JsExpression('function(item) { return item.text; }'),
+                            'templateSelection' => new JsExpression('function (item) { return item.text; }'),
+                        ],
+                ]);
+                ?>
+
+            </div>
+
+        </div>
+
+        <?php $this->endBlock(); ?>
+
         <?php $this->beginBlock('main'); ?>
 
         <p>
-
-            <!-- attribute shippingInstruction_id -->
-            <?=
-            // generated by schmunk42\giiant\generators\crud\providers\core\RelationProvider::activeField
-            $form->field($model, 'shippingInstruction_id')->dropDownList(
-                \yii\helpers\ArrayHelper::map(app\models\ShippingInstruction::find()->all(), 'id', 'id'),
-                [
-                    'prompt' => 'Select',
-                    'disabled' => (isset($relAttributes) && isset($relAttributes['shippingInstruction_id'])),
-                ]
-            );
-            ?>
-
-            <!-- attribute stuffingLocation_id -->
-            <?=
-            // generated by schmunk42\giiant\generators\crud\providers\core\RelationProvider::activeField
-            $form->field($model, 'stuffingLocation_id')->dropDownList(
-                \yii\helpers\ArrayHelper::map(app\models\StuffingLocation::find()->all(), 'id', 'name'),
-                [
-                    'prompt' => 'Select',
-                    'disabled' => (isset($relAttributes) && isset($relAttributes['stuffingLocation_id'])),
-                ]
-            );
-            ?>
-
-            <!-- attribute driver_id -->
-            <?=
-            // generated by schmunk42\giiant\generators\crud\providers\core\RelationProvider::activeField
-            $form->field($model, 'driver_id')->dropDownList(
-                \yii\helpers\ArrayHelper::map(app\models\Profile::find()->all(), 'id', 'name'),
-                [
-                    'prompt' => 'Select',
-                    'disabled' => (isset($relAttributes) && isset($relAttributes['driver_id'])),
-                ]
-            );
-            ?>
-
-            <!-- attribute supervisor_id -->
-            <?=
-            // generated by schmunk42\giiant\generators\crud\providers\core\RelationProvider::activeField
-            $form->field($model, 'supervisor_id')->dropDownList(
-                \yii\helpers\ArrayHelper::map(app\models\TruckSupervisor::find()->all(), 'id', 'name'),
-                [
-                    'prompt' => 'Select',
-                    'disabled' => (isset($relAttributes) && isset($relAttributes['supervisor_id'])),
-                ]
-            );
-            ?>
-
-            <!-- attribute stuffingDate -->
-            <?=
-            // generated by fredyns\suite\giiant\crud\providers\extensions\DateProvider::activeField
-            $form
-                ->field($model, 'stuffingDate')
-                // TODO: must configured properly together with model
-                //->widget(\yii\jui\DatePicker::classname(),
-                //    [
-                //        'dateFormat' => 'yyyy-MM-dd',
-                //    ])
-            ?>
 
             <!-- attribute containerNumber -->
             <?=
@@ -100,18 +206,94 @@ use dmstr\bootstrap\Tabs;
             <?=
             $form->field($model, 'sealNumber')->textInput(['maxlength' => true])
             ?>
+
+            <!-- attribute stuffingDate -->
+            <?=
+                $form->field($model, 'stuffingDate')
+                ->widget(\yii\jui\DatePicker::classname(),
+                    [
+                    'dateFormat' => 'yyyy-MM-dd',
+            ]);
+            ?>
+
+            <!-- attribute stuffingLocation_id -->
+            <?=
+                $form
+                ->field($model, 'stuffingLocation_id')
+                ->widget(Select2::classname(),
+                    [
+                    'initValueText' => ArrayHelper::getValue($model, 'stuffingLocation.name', '-'),
+                    'options' => ['placeholder' => 'mencari lokasi stuffing ...'],
+                    'pluginOptions' => [
+                        'allowClear' => true,
+                        'tags' => true,
+                        'minimumInputLength' => 3,
+                        'language' => [
+                            'errorLoading' => new JsExpression("function () { return 'menunggu hasil...'; }"),
+                        ],
+                        'ajax' => [
+                            'url' => Url::to(['/api/stuffing-location/list']),
+                            'dataType' => 'json',
+                            'data' => new JsExpression('function(params) { return {q:params.term}; }')
+                        ],
+                        'escapeMarkup' => new JsExpression('function (markup) { return markup; }'),
+                        'templateResult' => new JsExpression('function(item) { return item.text; }'),
+                        'templateSelection' => new JsExpression('function (item) { return item.text; }'),
+                    ],
+            ]);
+            ?>
+
+            <!-- attribute driver_id -->
+            <?=
+            // generated by schmunk42\giiant\generators\crud\providers\core\RelationProvider::activeField
+            $form->field($model, 'driver_id')->dropDownList(
+                \yii\helpers\ArrayHelper::map(app\models\Profile::find()->all(), 'id', 'name'),
+                [
+                'prompt' => 'Select',
+                'disabled' => (isset($relAttributes) && isset($relAttributes['driver_id'])),
+                ]
+            );
+            ?>
+
+            <!-- attribute supervisor_id -->
+            <?=
+                $form
+                ->field($model, 'supervisor_id')
+                ->widget(Select2::classname(),
+                    [
+                    'initValueText' => ArrayHelper::getValue($model, 'supervisor.name', '-'),
+                    'options' => ['placeholder' => 'mencari mandor ...'],
+                    'pluginOptions' => [
+                        'allowClear' => true,
+                        'tags' => true,
+                        'minimumInputLength' => 3,
+                        'language' => [
+                            'errorLoading' => new JsExpression("function () { return 'menunggu hasil...'; }"),
+                        ],
+                        'ajax' => [
+                            'url' => Url::to(['/api/truck-supervisor/list']),
+                            'dataType' => 'json',
+                            'data' => new JsExpression('function(params) { return {q:params.term}; }')
+                        ],
+                        'escapeMarkup' => new JsExpression('function (markup) { return markup; }'),
+                        'templateResult' => new JsExpression('function(item) { return item.text; }'),
+                        'templateSelection' => new JsExpression('function (item) { return item.text; }'),
+                    ],
+            ]);
+            ?>
+
         </p>
 
         <?php $this->endBlock(); ?>
-        
+
         <?=
         Tabs::widget([
             'encodeLabels' => false,
             'items' => [
                 [
-                    'label'   => Yii::t('app', 'JobContainer'),
+                    'label' => Yii::t('app', 'JobContainer'),
                     'content' => $this->blocks['main'],
-                    'active'  => true,
+                    'active' => true,
                 ],
             ],
         ]);
@@ -123,10 +305,10 @@ use dmstr\bootstrap\Tabs;
 
         <?=
         Html::submitButton(
-            '<span class="glyphicon glyphicon-check"></span> ' .
+            '<span class="glyphicon glyphicon-check"></span> '.
             ($model->isNewRecord ? 'Create' : 'Save'),
             [
-            'id' => 'save-' . $model->formName(),
+            'id' => 'save-'.$model->formName(),
             'class' => 'btn btn-success'
             ]
         );
@@ -137,4 +319,39 @@ use dmstr\bootstrap\Tabs;
     <?php ActiveForm::end(); ?>
 
 </div>
+
+<script>
+
+    function shipper_check()
+    {
+        shipperInput = $('#<?= $model->formName() ?>-shipper_id').val();
+
+        if (shipperInput && isNaN(shipperInput))
+        {
+            $('#input-shipperdetail').show('blind');
+        } else
+        {
+            $('#input-shipperdetail').hide('blind');
+        }
+    }
+
+</script>
+
+<?php
+$js = <<<JS
+
+	$(function () {
+        $('#input-shipperdetail').hide();
+        $('#input-shipper').hide();
+        $('#input-newsi').hide();
+        $('#input-picksi').show();
+
+        $('select').on('select2:select', function (evt) {
+            shipper_check();
+        });
+	});
+
+JS;
+
+$this->registerJs($js, \yii\web\View::POS_READY);
 
