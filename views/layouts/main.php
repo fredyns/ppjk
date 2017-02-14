d56336cf76c1e9d42f712fc345a7d0d31b1d1a80
Fredy Nurman Saleh <email@fredyns.net>
2017-02-14 17:39:35 +0700

    top nav layout

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,88 +1,126 @@
 <?php
+
+use yii\bootstrap\Nav;
+use yii\helpers\Html;
+use yii\helpers\Url;
+
 /* @var $this \yii\web\View */
 /* @var $content string */
 
-use yii\helpers\Html;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
-use yii\widgets\Breadcrumbs;
-use app\assets\AppAsset;
-use dmstr\widgets\Alert;
 
-AppAsset::register($this);
-?>
-<?php $this->beginPage() ?>
-<!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>">
-    <head>
-        <meta charset="<?= Yii::$app->charset ?>">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <?= Html::csrfMetaTags() ?>
-        <title><?= Html::encode($this->title) ?></title>
-        <?php $this->head() ?>
-    </head>
-    <body>
-        <?php $this->beginBody() ?>
+if (Yii::$app->controller->action->id === 'login') {
+    /**
+     * Do not use this code in your template. Remove it.
+     * Instead, use the code  $this->layout = '//main-login'; in your controller.
+     */
+    echo $this->render(
+        'adminlte/login', ['content' => $content]
+    );
+} else {
 
-        <div class="wrap">
-            <?php
-            NavBar::begin([
-                'brandLabel' => Html::img(
-                    '@web/jasco/logo.png',
-                    [
-                    'alt' => Yii::$app->name,
-                    'style' => 'length: 70%; width: 70%;',
-                    ]
-                ),
-                'brandUrl' => Yii::$app->homeUrl,
-                'brandOptions' => [
-                    'style' => 'margin: 0; padding: 0;',
-                ],
-                'options' => [
-                    'class' => 'navbar-inverse navbar-fixed-top',
-                ],
-            ]);
-            echo Nav::widget([
-                'options' => ['class' => 'navbar-nav navbar-right'],
-                'items' => [
-                    ['label' => 'Home', 'url' => ['/site/index']],
-                    ['label' => 'About', 'url' => ['/site/about']],
-                    [
-                        'label' => 'Login',
-                        'url' => ['/user/security/login'],
-                        'visible' => Yii::$app->user->isGuest,
-                    ],
-                    [
-                        'label' => 'Logout',
-                        'url' => ['/user/security/logout'],
-                        'visible' => !Yii::$app->user->isGuest,
-                        'linkOptions' => ['data-method' => 'post'],
-                    ],
-                ],
-            ]);
-            NavBar::end();
-            ?>
+    if (class_exists('backend\assets\AppAsset')) {
+        backend\assets\AppAsset::register($this);
+    } else {
+        app\assets\AppAsset::register($this);
+    }
 
-            <div class="container">
+    dmstr\web\AdminLteAsset::register($this);
+
+    $directoryAsset = Yii::$app->assetManager->getPublishedUrl('@vendor/almasaeed2010/adminlte/dist');
+    ?>
+    <?php $this->beginPage() ?>
+    <!DOCTYPE html>
+    <html lang="<?= Yii::$app->language ?>">
+        <head>
+            <meta charset="<?= Yii::$app->charset ?>"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <?= Html::csrfMetaTags() ?>
+            <title><?= Html::encode($this->title) ?></title>
+            <?php $this->head() ?>
+        </head>
+        <body class="hold-transition skin-blue layout-top-nav">
+            <?php $this->beginBody() ?>
+            <div class="wrapper">
+                <header class="main-header">
+                    <nav class="navbar navbar-static-top">
+                        <div class="container">
+                            <div class="navbar-header">
+                                <?php
+                                $logoMini = '<span class="logo-mini">'.Yii::$app->params['initials'].'</span>';
+                                $logoLg = '<span class="logo-lg">'
+                                    .Html::img(
+                                        '@web/jasco/logo.png',
+                                        [
+                                        'alt' => Yii::$app->name,
+                                        'style' => 'length: 70%; width: 70%;',
+                                        ]
+                                    )
+                                    .'</span>';
+
+                                echo Html::a($logoMini.$logoLg, Yii::$app->homeUrl, ['class' => 'logo'])
+                                ?>
+                                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+                                    <i class="fa fa-bars"></i>
+                                </button>
+                            </div>
+
+                            <!-- Collect the nav links, forms, and other content for toggling -->
+                            <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
+                                <?=
+                                Nav::widget([
+                                    'options' => ['class' => 'navbar-nav'],
+                                    'items' => [
+                                        ['label' => 'Home', 'url' => ['/site/index']],
+                                        ['label' => 'Services', 'url' => ['/site/about']],
+                                        ['label' => 'About', 'url' => ['/site/about']],
+                                    ],
+                                ]);
+                                ?>
+                                &nbsp;
+                                <form action="<?= Url::to(['search']); ?>" method="get" class="navbar-form navbar-left" role="search">
+                                    <div class="form-group">
+                                        <input type="text" name="" class="form-control" id="navbar-search-input" placeholder="Search container">
+                                    </div>
+                                </form>
+                            </div>
+                            <!-- /.navbar-collapse -->
+                            <!-- Navbar Right Menu -->
+                            <div class="navbar-custom-menu">
+                                <?=
+                                Nav::widget([
+                                    'options' => ['class' => 'navbar-nav navbar-right'],
+                                    'items' => [
+                                        [
+                                            'label' => 'Login',
+                                            'url' => ['/user/security/login'],
+                                            'visible' => Yii::$app->user->isGuest,
+                                        ],
+                                        [
+                                            'label' => 'Logout',
+                                            'url' => ['/user/security/logout'],
+                                            'visible' => !Yii::$app->user->isGuest,
+                                            'linkOptions' => ['data-method' => 'post'],
+                                        ],
+                                    ],
+                                ]);
+                                ?>
+                            </div>
+                            <!-- /.navbar-custom-menu -->
+                        </div>
+                        <!-- /.container-fluid -->
+                    </nav>
+                </header>
+
                 <?=
-                Breadcrumbs::widget([
-                    'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-                ])
+                $this->render(
+                    'adminlte/content.php', ['content' => $content, 'directoryAsset' => $directoryAsset]
+                )
                 ?>
-                <?= Alert::widget() ?>
-                <?= $content ?>
+
             </div>
-        </div>
 
-        <footer class="footer">
-            <div class="container">
-                <p class="pull-left">&copy; <?= Yii::$app->name.' '.date('Y') ?></p>
-
-                <p class="pull-right"><?= Yii::powered() ?></p>
-            </div>
-        </footer>
-
-        <?php $this->endBody() ?>
-    </body>
-</html>
-<?php $this->endPage() ?>
+            <?php $this->endBody() ?>
+        </body>
+    </html>
+    <?php $this->endPage() ?>
+<?php } ?>
