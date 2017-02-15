9b122a78f3656ccf8d01eb32db0cc3791d4cbfc0
Fredy Nurman Saleh <email@fredyns.net>
2017-02-15 10:55:19 +0700

    page: services

---
+++ views/site/page/services.php
@@ -0,0 +1,12 @@
+<?php
+
+use yii\helpers\Html;
+?>
+<p class="itemFullText">
+    <?= Html::img('@web/jasco/services/shipping-services.png'); ?><br/>
+    <?= Html::img('@web/jasco/services/airfreight.png'); ?><br/>
+    <?= Html::img('@web/jasco/services/custom-clearance.png'); ?><br/>
+    <?= Html::img('@web/jasco/services/trucking.png'); ?><br/>
+    <?= Html::img('@web/jasco/services/warehouse.png'); ?><br/>
+    <?= Html::img('@web/jasco/services/services.png'); ?><br/>
+</p>
