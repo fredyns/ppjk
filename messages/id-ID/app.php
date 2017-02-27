e5770784942c87e76dcb8d3cabcfb707228371d9
fredy <email@fredyns.net>
2017-02-27 14:06:35 +0700

    bahasa indonesia

---
+++ messages/id-ID/app.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Translation map for id-ID
+ */
+return [
+    // global
+    'Member Since {0, date}' => 'Terdaftar sejak {0, date}',
+    // models
+    'Company Profile' => 'Daftar Perusahaan',
+    'Company Profiles' => 'Daftar Perusahaan',
+    'Container Port' => 'Pelabuhan',
+    'Container Ports' => 'Pelabuhan',
+    'Shipping Instruction' => 'SI',
+    'Shipping Instructions' => 'SI',
+    'Job Container' => 'Job Container',
+    'Job Containers' => 'Job Container',
+    'Personel' => 'Personil',
+    'Personels' => 'Personil',
+    'Profile' => 'Profil',
+    'Profiles' => 'Profil',
+    'Shipping' => 'Shipping',
+    'Shippings' => 'Shipping',
+    'Shipping Instruction' => 'SI',
+    'Shipping Instructions' => 'SI',
+    'Stuffing Location' => 'Lokasi Stuffing',
+    'Stuffing Locations' => 'Lokasi Stuffing',
+    'Truck Supervisor' => 'Mandor',
+    'Truck Supervisors' => 'Mandor',
+    // label
+    'Company' => 'Perusahaan',
+    'Search Result' => 'Hasil Pencarian',
+    'Search' => 'Pencarian',
+    'Search Containers' => 'Pencarian Container',
+    '____' => '____',
+    '____' => '____',
+    '____' => '____',
+];
