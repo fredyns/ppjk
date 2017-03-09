bd40f80eb23b5a6d2475a81c66a8bdd4c48016d3
fredy <email@fredyns.net>
2017-03-09 14:28:23 +0700

    cmd tool

---
+++ setcommiter.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+git filter-branch -f --env-filter '
+OLD_EMAIL="fredy@fredy-B490"
+OLD_EMAIL2="fredy@fredy-Lenovo-B490"
+CORRECT_NAME="Fredy Nurman Saleh"
+CORRECT_EMAIL="email@fredyns.net"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL2" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL2" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+
+git push --force --tags origin 'refs/heads/*'
