f6fe0bf723d86b77f8ea9cfdad3c903d357d4cbd
fredy <email@fredyns.net>
2017-03-15 17:19:46 +0700

    algolia places API

--- views/container-port/_form.php
+++ views/container-port/_form.php
@@ -73,3 +73,10 @@ use dmstr\bootstrap\Tabs;
 
 </div>
 
+<script src="https://cdn.jsdelivr.net/places.js/1/places.min.js"></script>
+<script>
+    var placesAutocomplete = places({
+        container: document.querySelector('#containerportform-name')
+    });
+</script>
+
