ae9f72eaf9fd571edad9c98341e03c2e29b0ab1e
fredy <email@fredyns.net>
2017-03-25 17:24:02 +0700

    upload picture

--- views/user/admin/_profile.php
+++ views/user/admin/_profile.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * This file is part of the Dektrium project
  *
@@ -17,22 +16,49 @@ use yii\helpers\Html;
  * @var dektrium\user\models\User       $user
  * @var dektrium\user\models\Profile    $profile
  */
-
 ?>
 
 <?php $this->beginContent('@dektrium/user/views/admin/update.php', ['user' => $user]) ?>
 
-<?php $form = ActiveForm::begin([
-    'layout' => 'horizontal',
-    'enableAjaxValidation' => true,
-    'enableClientValidation' => false,
-    'fieldConfig' => [
-        'horizontalCssClasses' => [
-            'wrapper' => 'col-sm-9',
+<?php
+$form = ActiveForm::begin([
+        'layout' => 'horizontal',
+        'enableAjaxValidation' => true,
+        'enableClientValidation' => false,
+        'fieldConfig' => [
+            'horizontalCssClasses' => [
+                'wrapper' => 'col-sm-9',
+            ],
         ],
-    ],
-]); ?>
+    ]);
+?>
 
+<!-- attribute picture -->
+
+<?php if (!empty($profile->picture_id)): ?>
+    <div class="form-group field-profile-display">
+        <label class="control-label col-sm-3" for="profile-picture">&nbsp;</label>
+        <div class="col-sm-9">
+            <p>
+                <?=
+                Html::img(
+                    ['/file', 'id' => $profile->picture_id],
+                    [
+                    'class' => 'img-responsive',
+                    'alt' => 'picture',
+                    ]
+                )
+                ?>
+            </p>
+        </div>
+
+    </div>
+
+    <div class="clearfix"></div>
+
+<?php endif; ?>
+
+<?= $form->field($profile, 'picture')->label('Picture')->fileInput(); ?>
 <?= $form->field($profile, 'name') ?>
 <?= $form->field($profile, 'public_email') ?>
 <?= $form->field($profile, 'website') ?>
