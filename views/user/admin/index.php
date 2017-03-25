318c764cf30b858bb272a25ee0419cf26ef0e78d
fredy <email@fredyns.net>
2017-03-25 15:56:28 +0700

    profile link

--- views/user/admin/index.php
+++ views/user/admin/index.php
@@ -20,7 +20,7 @@ use yii\widgets\Pjax;
  * @var ActiveDataProvider $dataProvider
  * @var UserSearch $searchModel
  */
-$this->title                   = Yii::t('user', 'Manage users');
+$this->title = Yii::t('user', 'Manage users');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
@@ -31,83 +31,76 @@ $this->params['breadcrumbs'][] = $this->title;
 <?=
 GridView::widget([
     'dataProvider' => $dataProvider,
-    'filterModel'  => $searchModel,
-    'layout'       => "{items}\n{pager}",
-    'columns'      => [
-        'username',
+    'filterModel' => $searchModel,
+    'layout' => "{items}\n{pager}",
+    'columns' => [
+        [
+            'attribute' => 'username',
+            'format' => 'html',
+            'value' => function ($model) {
+                return Html::a($model->username, ['/user/profile/show', 'id' => $model->id]);
+            },
+        ],
         'email:email',
-            [
+        [
             'attribute' => 'registration_ip',
-            'value'     => function ($model)
-            {
+            'value' => function ($model) {
                 return $model->registration_ip == null ? '<span class="not-set">'.Yii::t('user', '(not set)').'</span>' : $model->registration_ip;
             },
             'format' => 'html',
         ],
-            [
+        [
             'attribute' => 'created_at',
-            'value'     => function ($model)
-            {
-                if (extension_loaded('intl'))
-                {
+            'value' => function ($model) {
+                if (extension_loaded('intl')) {
                     return Yii::t('user', '{0, date, MMMM dd, YYYY HH:mm}', [$model->created_at]);
-                }
-                else
-                {
+                } else {
                     return date('Y-m-d G:i:s', $model->created_at);
                 }
             },
         ],
-            [
+        [
             'header' => Yii::t('user', 'Confirmation'),
-            'value'  => function ($model)
-            {
-                if ($model->isConfirmed)
-                {
+            'value' => function ($model) {
+                if ($model->isConfirmed) {
                     return '<div class="text-center">
                                 <span class="text-success">'.Yii::t('user', 'Confirmed').'</span>
                             </div>';
-                }
-                else
-                {
+                } else {
                     return Html::a(Yii::t('user', 'Confirm'), ['confirm', 'id' => $model->id],
                             [
-                            'class'        => 'btn btn-xs btn-success btn-block',
-                            'data-method'  => 'post',
+                            'class' => 'btn btn-xs btn-success btn-block',
+                            'data-method' => 'post',
                             'data-confirm' => Yii::t('user', 'Are you sure you want to confirm this user?'),
                     ]);
                 }
             },
-            'format'  => 'raw',
+            'format' => 'raw',
             'visible' => Yii::$app->getModule('user')->enableConfirmation,
         ],
-            [
+        [
             'header' => Yii::t('user', 'Block status'),
-            'value'  => function ($model)
-            {
-                if ($model->isBlocked)
-                {
+            'value' => function ($model) {
+                if ($model->isBlocked) {
                     return Html::a(Yii::t('user', 'Unblock'), ['block', 'id' => $model->id],
                             [
-                            'class'        => 'btn btn-xs btn-success btn-block',
-                            'data-method'  => 'post',
+                            'class' => 'btn btn-xs btn-success btn-block',
+                            'data-method' => 'post',
                             'data-confirm' => Yii::t('user', 'Are you sure you want to unblock this user?'),
                     ]);
-                }
-                else
-                {
+                } else {
                     return Html::a(Yii::t('user', 'Block'), ['block', 'id' => $model->id],
                             [
-                            'class'        => 'btn btn-xs btn-danger btn-block',
-                            'data-method'  => 'post',
+                            'class' => 'btn btn-xs btn-danger btn-block',
+                            'data-method' => 'post',
                             'data-confirm' => Yii::t('user', 'Are you sure you want to block this user?'),
                     ]);
                 }
             },
             'format' => 'raw',
         ],
-            [
-            'class'    => 'yii\grid\ActionColumn',
+        [
+            'class' => 'yii\grid\ActionColumn',
             'template' => '{update} {delete}',
         ],
     ],
