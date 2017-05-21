21d7a4f644bfa5369e16f251fe89743f7c8e7c9a
fredy <email@fredyns.net>
2017-05-21 19:51:48 +0700

    change title

--- views/site/page/chart.php
+++ views/site/page/chart.php
@@ -11,7 +11,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <div class="col-md-12">
         <div class="box">
             <div class="box-header with-border">
-                <h3 class="box-title">Daily Stuffing Report</h3>
+                <h3 class="box-title">Global Stuffing Report</h3>
 
                 <div class="box-tools pull-right">
                     <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
