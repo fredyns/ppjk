5d4cd3a99fc9667fbf8a62196b9939760f19cdb2
fredy <email@fredyns.net>
2017-05-22 19:04:47 +0700

    homepage

---
+++ views/site/page/home.php
@@ -0,0 +1,52 @@
+<?php
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+
+$this->title = Yii::$app->name;
+?>
+
+<div class="site-index">
+
+    <div class="content box">
+
+        <h1>
+            PT. Jasco Logistics
+        </h1>
+
+        <div style="text-align: justify; font-size: 14px;">
+            has been offering first-class transport and logistics solutions for more than 10 years.
+            We building a strong global network and ensuring close customer relationships and market focus.
+            <br/>
+            <br/>
+            Jasco has full Indonesia Freight Forwarding licensed,
+            and we are managed by the solid management team which consists of capable personnel,
+            who have extensive, and experience in the freight forwarding industry,
+            so we can offer quick and flexible transport solutions tailored to meet your needs for all types of goods.
+            <br/>
+            <br/>
+            JASCO LOGISTICS has office at major cities in Indonesia,
+            such as : SEMARANG (covering Yogya, Solo, Jepara), JAKARTA and SURABAYA.
+            <br/>
+            <br/>
+            Our full range of transportation and logistics services include air and ocean freight,
+            domestic distribution, warehousing, trucking,
+            as well as quality insurance to protect your goods.
+            &nbsp;
+        </div>
+
+        <div style="text-align: center;">
+            <?= Html::img('@web/jasco/images/we-deliver-all-your-shipments-around-the-world.jpg'); ?>
+        </div>
+
+        <div style="text-align: center;">
+            <?= Html::img('@web/jasco/images/wo.png'); ?>
+        </div>
+
+        <div style="text-align: center;">
+            <?= Html::img('@web/jasco/images/save-delivery.jpg'); ?>
+        </div>
+
+    </div>
+</div>
